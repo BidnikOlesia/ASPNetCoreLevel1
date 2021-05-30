31c8ebdbb7f6fa5c39634e3bde210789c7b5c23b
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-05-30 04:38:54 -0700

    Добавление представлений Checkout, Contactus, Login, ProductDetails, Shop

--- WebStore/Controllers/Home.cs
+++ WebStore/Controllers/Home.cs
@@ -46,5 +46,15 @@ namespace WebStore.Controllers
 
         public IActionResult Cart() => View();
 
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
+
     }
 }
