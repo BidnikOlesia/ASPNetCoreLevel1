292ff12c12c64e25db2bdbe2bf787b4d3428fa9c
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-02 10:05:28 -0700

    Добавлен интерфейс и сервис управления сотрудниками

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()=> View(_Employees);
+
+        public IActionResult EmployeeDetails(int id)
+        {
+            Employee employee = _Employees.Where(x => x.Id == id).FirstOrDefault();
+            if (employee == null)
+                return NotFound();
+            return View(employee);
+        }
+    }
+}
