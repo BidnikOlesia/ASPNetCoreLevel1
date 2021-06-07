8bf58974cff0eed996d5aeafa04b034c630d4cb5
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-07 11:44:38 -0700

    Добавление модели и сервиса каталога товаров

--- WebStore/Services/Interfaces/IProductData.cs
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 
 namespace WebStore.Services.Interfaces
@@ -11,5 +12,7 @@ namespace WebStore.Services.Interfaces
         IEnumerable<Brand> GetBrands();
 
         IEnumerable<Section> GetSections();
+
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null);
     }
 }
