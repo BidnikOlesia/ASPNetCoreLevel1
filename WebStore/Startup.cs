ae16e2412c7e11b7554712083b246dbd48a3e8f2
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-12 03:54:28 -0700

    Подключение контекста бд в основном приложении в качестве сервиса

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.DAL.Context;
 using WebStore.Services;
 using WebStore.Services.Interfaces;
 
@@ -22,6 +24,8 @@ namespace WebStore
         }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(opt=> opt.UseSqlServer(Configuration.GetConnectionString("MSSQL")));
+            
             services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
 
             services.AddSingleton<IProductData, InMemoryProductData>();
