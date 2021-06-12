aea7a0e6d2b74c6d59d2a1b4da0c8708e7868521
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-12 03:02:11 -0700

    Подготовка сущностей для размещения в БД

--- WebStore.Domain/Entities/Base/NamedEntity.cs
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class NamedEntity:Entity, INamedEntity
     {
+        [Required]
         public string Name { get; set; }
     }
 }
--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
@@ -13,5 +14,10 @@ namespace WebStore.Domain.Entities
         public int Order { get; set; }
 
         public int? ParentId { get; set; }
+
+        [ForeignKey(nameof(ParentId))]
+        public Section Parent { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
