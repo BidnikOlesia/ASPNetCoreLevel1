5c40ee2b147cb2c409d1e9105cb1fe644dd7347f
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-21 10:18:42 -0700

    Миграция БД

--- WebStore.Domain/Entities/Employee.cs
+++ WebStore.Domain/Entities/Employee.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities
 {
-    public class Employee
+    public class Employee:Entity
     {
-        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string FirstName { get; set; }
+
+        [MaxLength(100)]
         public string LastName { get; set; }
+
+        [MaxLength(100)]
         public string MiddleName { get; set; }
+
         public int Age { get; set; }
+
         public DateTime EmploymentDate { get; set; }
+
         public string Position { get; set; }
     }
 }
