4269982d416688b5e54ee85a56c21b50f81393d9
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-22 08:20:32 -0700

    Сущности заказа в БД

---
+++ WebStore.Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities.Orders
+{
+    public class OrderItem:Entity
+    {
+        [Required]
+        public Order Order { get; set; }
+
+        [Required]
+        public Product Product { get; set; }
+
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [NotMapped]
+        public decimal TotalItemPrice => Price * Quantity;
+    }
+}
