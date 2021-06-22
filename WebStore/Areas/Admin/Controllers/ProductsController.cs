56f985b60156cb981e46d3b0870d6635a61caf5e
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-22 07:16:57 -0700

    Заглушки методов редактирования и удаления товаров

---
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Authorize(Roles = Role.Administrators)]
+    public class ProductsController : Controller
+    {
+        private readonly IProductData productData;
+
+        public ProductsController(IProductData ProductData)=> productData = ProductData;
+
+        public IActionResult Index()=> View(productData.GetProducts());
+
+        public IActionResult Edit(int id) =>
+            productData.GetProductById(id) is { } product
+            ? View(product)
+            : NotFound();
+
+        public IActionResult Delete(int id) =>
+            productData.GetProductById(id) is { } product
+            ? View(product)
+            : NotFound();
+
+    }
+}
