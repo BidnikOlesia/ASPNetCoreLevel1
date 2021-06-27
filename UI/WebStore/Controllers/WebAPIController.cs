368644788e2d524a5060b05e17306f956b649285
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-06-27 11:47:45 -0700

    MVC контроллер для демонстрации работы клиента тeстового WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Interfaces.Services.TestAPI;
+
+namespace WebStore.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService valuesService;
+
+        public WebAPIController(IValuesService ValuesService)=> valuesService = ValuesService;
+
+        public IActionResult Index()
+        {
+            var values = valuesService.GetAll();
+            return View(values);
+        }
+    }
+}
