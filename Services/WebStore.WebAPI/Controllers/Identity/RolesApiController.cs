0ccaadea18e0abde98f4e1aa6653242745551a28
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-07-04 08:11:16 -0700

    Контроллеры управления ролями и пользователями

--- Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -25,5 +25,58 @@ namespace WebStore.WebAPI.Controllers.Identity
 
         [HttpGet("all")]
         public async Task<IEnumerable<Role>> GetAllRoles() => await _RolesStroe.Roles.ToArrayAsync();
+
+        [HttpPost]
+        public async Task<bool> CreateAsync(Role role)
+        {
+            var creation_result = await _RolesStroe.CreateAsync(role);
+            // Добавить логирование в случае Succeeded == false
+            return creation_result.Succeeded;
+        }
+
+        [HttpPut]
+        public async Task<bool> UpdateAsync(Role role)
+        {
+            var uprate_result = await _RolesStroe.UpdateAsync(role);
+            return uprate_result.Succeeded;
+        }
+
+        [HttpPost("Delete")]
+        public async Task<bool> DeleteAsync(Role role)
+        {
+            var delete_result = await _RolesStroe.DeleteAsync(role);
+            return delete_result.Succeeded;
+        }
+
+        [HttpPost("GetRoleId")]
+        public async Task<string> GetRoleIdAsync([FromBody] Role role) => await _RolesStroe.GetRoleIdAsync(role);
+
+        [HttpPost("GetRoleName")]
+        public async Task<string> GetRoleNameAsync([FromBody] Role role) => await _RolesStroe.GetRoleNameAsync(role);
+
+        [HttpPost("SetRoleName/{name}")]
+        public async Task<string> SetRoleNameAsync(Role role, string name)
+        {
+            await _RolesStroe.SetRoleNameAsync(role, name);
+            await _RolesStroe.UpdateAsync(role);
+            return role.Name;
+        }
+
+        [HttpPost("GetNormalizedRoleName")]
+        public async Task<string> GetNormalizedRoleNameAsync(Role role) => await _RolesStroe.GetNormalizedRoleNameAsync(role);
+
+        [HttpPost("SetNormalizedRoleName/{name}")]
+        public async Task<string> SetNormalizedRoleNameAsync(Role role, string name)
+        {
+            await _RolesStroe.SetNormalizedRoleNameAsync(role, name);
+            await _RolesStroe.UpdateAsync(role);
+            return role.NormalizedName;
+        }
+
+        [HttpGet("FindById/{id}")]
+        public async Task<Role> FindByIdAsync(string id) => await _RolesStroe.FindByIdAsync(id);
+
+        [HttpGet("FindByName/{name}")]
+        public async Task<Role> FindByNameAsync(string name) => await _RolesStroe.FindByNameAsync(name);
     }
 }
