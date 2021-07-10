652014fb787133e1f9b8906e153d5c9cac312aa8
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-07-10 06:12:23 -0700

    Сервис хранения данных корзины в cookies

---
+++ Services/WebStore.Services/Services/InCookies/InCookiesCartStore.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Services.Services.InCookies
+{
+    public class InCookiesCartStore: ICartStore
+    {
+        private readonly string cartName;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public Cart Cart
+        {
+            get
+            {
+                var context = httpContextAccessor.HttpContext;
+                var cookies = context!.Response.Cookies;
+
+                var cart_cookie = context.Request.Cookies[cartName];
+                if (cart_cookie is null)
+                {
+                    var cart = new Cart();
+                    cookies.Append(cartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+
+                ReplaceCookies(cookies, cart_cookie);
+                return JsonConvert.DeserializeObject<Cart>(cart_cookie);
+            }
+            set => ReplaceCookies(httpContextAccessor.HttpContext.Response.Cookies, JsonConvert.SerializeObject(value));
+        }
+
+        private void ReplaceCookies(IResponseCookies cookies, string cookie)
+        {
+            cookies.Delete(cartName);
+            cookies.Append(cartName, cookie);
+        }
+
+        public InCookiesCartStore(IHttpContextAccessor HttpContextAccessor)
+        {
+            httpContextAccessor = HttpContextAccessor;
+
+            var user = httpContextAccessor.HttpContext!.User;
+            var user_name = user.Identity!.IsAuthenticated ? $"-{user.Identity.Name}" : null;
+
+            cartName = $"WebStore.Cart{user_name}";
+        }
+    }
+}
