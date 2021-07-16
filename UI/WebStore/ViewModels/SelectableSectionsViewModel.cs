adabc755363481641c6beeb14dbbb40c0c1df366
Olesia Bidnik <olesia.bidnik@outlook.com>
2021-07-16 05:11:13 -0700

    Выделение активной секции

---
+++ UI/WebStore/ViewModels/SelectableSectionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.ViewsModels;
+
+namespace WebStore.ViewModels
+{
+    public class SelectableSectionsViewModel
+    {
+        public IEnumerable<SectionViewModel> Sections { get; init; }
+
+        public int? SectionId { get; init; }
+
+        public int? ParentSectionId { get; init; }
+    }
+}
